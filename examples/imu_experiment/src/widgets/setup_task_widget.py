0b0abc1f5c020e48df5eec922d2ac8d9c81daa5c
Eden Redman <46800990+Existentialist-Robot@users.noreply.github.com>
2023-08-20 19:27:35 -0600

    Add proper buttonage for imu setup task

--- examples/imu_experiment/src/widgets/setup_task_widget.py
+++ examples/imu_experiment/src/widgets/setup_task_widget.py
@@ -6,8 +6,8 @@ from PyQt6.QtWidgets import QLabel
 from PyQt6.QtCore import Qt
 
 from .imu_plot_widget import ImuPlotWidget
-from .imu_calibration_widget import ImuCalibrationWidget
 from .imu_connection_widget import ImuConnectionWidget
+from .imu_calibration_widget import ImuCalibrationWidget
 
 from typing import NoReturn
 
@@ -20,6 +20,8 @@ class SetupTaskWidget(QWidget):
 
         top_layout = QHBoxLayout()
 
+        button_layout = QHBoxLayout()
+        
         layout = QVBoxLayout()
 
         self._title_label = QLabel(
@@ -38,7 +40,13 @@ class SetupTaskWidget(QWidget):
         self._imu_plot_widget = ImuPlotWidget()
         layout.addWidget(self._imu_plot_widget, 5)
 
-        self._next_task_button = QPushButton("")
+        self._previous_task_button = QPushButton(text="<- Change IMU Associations")
+        button_layout.addWidget(self._previous_task_button,alignment=Qt.AlignmentFlag.AlignLeft)
+        
+        self._next_task_button = QPushButton(text="Data Collection ->")
+        button_layout.addWidget(self._next_task_button,alignment=Qt.AlignmentFlag.AlignRight)
+
+        layout.addLayout(button_layout, 0)
 
         self.setLayout(layout)
 
