8def6f7179d31843138e9c012e51e4f476334f09
Zach Selk <zrselk@gmail.com>
2026-02-25 12:06:30 -0700

    removed aggressive gitignore rule

---
+++ frontend/src/lib/components/transition.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { router } from "tinro";
+    import { fade } from "svelte/transition";
+  </script>
+  
+  {#key $router.path}
+    <div in:fade={{ duration: 700 }}>
+      <slot />
+    </div>
+  {/key}
+  
